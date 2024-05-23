d71041303c6c0b3e8df5e4c84b9221e4051b70b8
Marc Singleton <marcsingleton@berkeley.edu>
2024-05-23 11:57:42 -0700

    Added notes to network.js

--- content/posts/d3-markdown/network.js
+++ content/posts/d3-markdown/network.js
@@ -46,12 +46,26 @@ node.append("title")
     .text(d => d.id);
 
 // Add a drag behavior.
+/*
+# NOTES
+- Effectively binds dragging behavior to each element in a selection
+- This is an idiomatic construct of D3, so don't take it too literally
+- call on a selection accepts a function and applies that function to the selection as its first argument
+- drag produces a function that on being called binds event listeners to each element in the selection
+- Internally it uses the on method of the selection to do this binding, similar to how it would be done for other events
+- Dragging is a complicated behavior involving clicks and moves, so this idiom simplifies the DOM events into a convenient default behavior
+*/
 node.call(d3.drag()
     .on("start", dragstarted)
     .on("drag", dragged)
     .on("end", dragended));
 
 // Set the position attributes of links and nodes each time the simulation ticks.
+/*
+# NOTES
+- The simulation automatically runs on the data structure passed to it, updating the records' positions
+- This command links those updates that run on each "tick" to the positions of the links and nodes on the screen
+*/
 simulation.on("tick", () => {
 link
     .attr("x1", d => d.source.x)
@@ -64,24 +78,32 @@ node
     .attr("cy", d => d.y);
 });
 
+/*
+# NOTES
+- Drag events are different from DOM events since they're explicitly a D3 concept
+- Subjects are typically the datum joined to that element (not the element itself)
+- These events are then modifying the data records and not the links and nodes on the screen
+- fx and fy are special values in these structures that the simulation uses to "fix" the positions
+- If they're null, the dynamic positions (x, y) are free to vary; otherwise they're fixed to (fx, fy)
+*/
 // Reheat the simulation when drag starts, and fix the subject position.
 function dragstarted(event) {
-if (!event.active) simulation.alphaTarget(0.3).restart();
-event.subject.fx = event.subject.x;
-event.subject.fy = event.subject.y;
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    event.subject.fx = event.subject.x;
+    event.subject.fy = event.subject.y;
 }
 
 // Update the subject (dragged node) position during drag.
 function dragged(event) {
-event.subject.fx = event.x;
-event.subject.fy = event.y;
+    event.subject.fx = event.x;
+    event.subject.fy = event.y;
 }
 
 // Restore the target alpha so the simulation cools after dragging ends.
 // Unfix the subject position now that it’s no longer being dragged.
 function dragended(event) {
-if (!event.active) simulation.alphaTarget(0);
-event.subject.fx = null;
-event.subject.fy = null;
+    if (!event.active) simulation.alphaTarget(0);
+    event.subject.fx = null;
+    event.subject.fy = null;
 }
 }
